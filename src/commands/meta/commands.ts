a9a919e6c5e0a815934aa3dc918fc8970364adf7
Scott Walter <sjwalter@gmail.com>
2021-02-12 16:26:50 -0600

    meta:commands

--- src/commands/meta/commands.ts
+++ src/commands/meta/commands.ts
@@ -1,19 +1,39 @@
 import {Command, flags} from '@oclif/command'
+// import { table } from 'console';
+import config from '../../config'
+import * as https from 'https'
 
 export default class MetaCommands extends Command {
   static description = 'meta:commands'
 
+  private cmd = 'api-list_accessible_cmds'
+
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    account: flags.string({char: 'a', description: 'account to use'}),
+    format: flags.string({char: 'f', description: 'format to return'}),
+  };
 
   async run() {
-    // const {args, flags} = this.parse(DnsIndex)
+    const {flags} = this.parse(MetaCommands)
 
-    this.log(`hello from ${__filename.replace(process.cwd(), '..')}`)
+    https
+    .get(
+      config.url(this.cmd, String(flags.format), flags.account ? String(flags.account) : null),
+      resp => {
+        let data = ''
+
+        resp.on('data', chunk => {
+          data += chunk
+        })
+
+        resp.on('end', () => {
+          // The whole response has been received. Print out the result.
+          this.log(data)
+        })
+      }
+    )
+    .on('error', err => {
+      this.log(`Error: ${err.message}`)
+    })
   }
 }
