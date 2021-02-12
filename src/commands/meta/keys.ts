0ec90a9bd41bb14bef382efde0fc4e54ca742d5f
Scott Walter <sjwalter@gmail.com>
2021-02-12 17:29:50 -0600

    converted meta:keys to custom base class

--- src/commands/meta/keys.ts
+++ src/commands/meta/keys.ts
@@ -1,43 +1,19 @@
-import {Command, flags} from '@oclif/command'
-// import { table } from 'console';
-import config from '../../config'
-import * as https from 'https'
+import Command from '../../base'
 
-export default class MetaKeys extends Command {
+export class MetaKeys extends Command {
   static description = 'meta:keys'
 
   private cmd = 'api-list_keys'
 
   static flags = {
-    account: flags.string({char: 'a', description: 'account to use'}),
-    format: flags.string({char: 'f', description: 'format to return'}),
-  };
+    ...Command.flags,
+  }
 
-  async run() {
-    const {flags} = this.parse(MetaKeys)
+  parse_flags() {
+    return this.parse(MetaKeys)
+  }
 
-    https
-    .get(
-      config.url(
-        this.cmd,
-        String(flags.format),
-        flags.account ? String(flags.account) : null
-      ),
-      resp => {
-        let data = ''
-
-        resp.on('data', chunk => {
-          data += chunk
-        })
-
-        resp.on('end', () => {
-          // The whole response has been received. Print out the result.
-          this.log(data)
-        })
-      }
-    )
-    .on('error', err => {
-      this.log(`Error: ${err.message}`)
-    })
+  get_cmd() {
+    return this.cmd
   }
 }
